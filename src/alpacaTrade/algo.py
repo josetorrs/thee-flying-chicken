aabfd65cbc8a1e7d0b6c565fab6b024851cc289a
Jose Torres <josetorrs7@gmail.com>
2019-03-04 18:44:40 -0600

    changes to structure

--- src/alpacaTrade/algo.py
+++ src/alpacaTrade/algo.py
@@ -1,13 +1,107 @@
-import logging
 import argparse
 import pandas as pd
 import numpy as np
 import alpaca_trade_api as tradeapi
-from .Position import Position
-from .Quote import Quote
 
-logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.DEBUG)
+class Position():
+    """
+    The position object is used to track how many shares we have. We need to
+    keep track of this so our position size doesn't inflate beyond the level
+    we're willing to trade with. Because orders may sometimes be partially
+    filled, we need to keep track of how many shares are "pending" a buy or
+    sell as well as how many have been filled into our account.
+    """
+    def __init__(self):
+        self.orders_filled_amount = {}
+        self.pending_buy_shares = 0
+        self.pending_sell_shares = 0
+        self.total_shares = 0
+
+    def update_pending_buy_shares(self, quantity):
+        self.pending_buy_shares += quantity
+
+    def update_pending_sell_shares(self, quantity):
+        self.pending_sell_shares += quantity
+
+    def update_filled_amount(self, order_id, new_amount, side):
+        old_amount = self.orders_filled_amount[order_id]
+        if new_amount > old_amount:
+            if side == 'buy':
+                self.update_pending_buy_shares(old_amount - new_amount)
+                self.update_total_shares(new_amount - old_amount)
+            else:
+                self.update_pending_sell_shares(old_amount - new_amount)
+                self.update_total_shares(old_amount - new_amount)
+            self.orders_filled_amount[order_id] = new_amount
+
+    def remove_pending_order(self, order_id, side):
+        old_amount = self.orders_filled_amount[order_id]
+        if side == 'buy':
+            self.update_pending_buy_shares(old_amount - 100)
+        else:
+            self.update_pending_sell_shares(old_amount - 100)
+        del self.orders_filled_amount[order_id]
+
+    def update_total_shares(self, quantity):
+        self.total_shares += quantity
+
+class Quote():
+    """
+    We use Quote objects to represent the bid/ask spread. When we encounter a
+    'level change', a move of exactly 1 penny, we may attempt to make one
+    trade. Whether or not the trade is successfully filled, we do not submit
+    another trade until we see another level change.
+    Note: Only moves of 1 penny are considered eligible because larger moves
+    could potentially indicate some newsworthy event for the stock, which this
+    algorithm is not tuned to trade.
+    """
+
+    def __init__(self):
+        self.prev_bid = 0
+        self.prev_ask = 0
+        self.prev_spread = 0
+        self.bid = 0
+        self.ask = 0
+        self.bid_size = 0
+        self.ask_size = 0
+        self.spread = 0
+        self.traded = True
+        self.level_ct = 1
+        self.time = 0
+
+    def reset(self):
+        # Called when a level change happens
+        self.traded = False
+        self.level_ct += 1
+
+    def update(self, data):
+        # Update bid and ask sizes and timestamp
+        self.bid_size = data.bidsize
+        self.ask_size = data.asksize
+
+        # Check if there has been a level change
+        if (
+            self.bid != data.bidprice
+            and self.ask != data.askprice
+            and round(data.askprice - data.bidprice, 2) == .01
+        ):
+            # Update bids and asks and time of level change
+            self.prev_bid = self.bid
+            self.prev_ask = self.ask
+            self.bid = data.bidprice
+            self.ask = data.askprice
+            self.time = data.timestamp
+            # Update spreads
+            self.prev_spread = round(self.prev_ask - self.prev_bid, 3)
+            self.spread = round(self.ask - self.bid, 3)
+            print(
+                'Level change:', self.prev_bid, self.prev_ask,
+                self.prev_spread, self.bid, self.ask, self.spread
+            )
+            # If change is from one penny spread level to a different penny
+            # spread level, then initialize for new level (reset stale vars)
+            if self.prev_spread == 0.01:
+                self.reset()
 
 # Establish streaming connection
 conn = tradeapi.StreamConn()
@@ -79,7 +173,7 @@ def run(args):
                     api.cancel_order(o.id)
                     position.update_pending_buy_shares(100)
                     position.orders_filled_amount[o.id] = 0
-                    print('Buy at', quote.ask, flush=True)
+                    print('Buy at', quote.ask)
                     quote.traded = True
                 except Exception as e:
                     print(e)
@@ -101,7 +195,7 @@ def run(args):
                     api.cancel_order(o.id)
                     position.update_pending_sell_shares(100)
                     position.orders_filled_amount[o.id] = 0
-                    print('Sell at', quote.bid, flush=True)
+                    print('Sell at', quote.bid)
                     quote.traded = True
                 except Exception as e:
                     print(e)
@@ -137,9 +231,7 @@ def run(args):
         ['trade_updates', tc, qc]
     )
 
-
 if __name__ == '__main__':
-
     parser = argparse.ArgumentParser()
     parser.add_argument(
         '--symbol', type=str, default='SPY',
